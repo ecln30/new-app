e29171bb7379c6841810e882742edb15459214b2
ecln30 <ecln30@gmail.com>
2022-09-08 23:20:14 +0900

    first and push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes } from "react-router-dom";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Reset from './pages/Reset'
+import Dashboard from './pages/Dashboard'
 
+// main function App
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route  path="/" element={<Login />} />
+          <Route  path="register" element={<Register />} />
+          <Route  path="reset" element={<Reset />} />
+          <Route  path="dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
